ab73d3d1752216cc0e47228bd736fc05a3901d22
Basith <70204001+MuhammedBasith@users.noreply.github.com>
2022-06-27 02:03:52 +0530

    initial

---
+++ main.py
@@ -0,0 +1,6 @@
+from move_comparison import compare_positions
+
+benchmark_video = 'dance_videos/gym/bench.mp4'
+user_video = 'dance_videos/gym/right.mp4'
+
+compare_positions(benchmark_video, user_video)
